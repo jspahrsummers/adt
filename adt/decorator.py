e2eb2f72fe8306696385ed50e6102d83e857a3e2
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-06-26 22:53:05 +0100

    Basic, untyped ADT implementation

--- adt/decorator.py
+++ adt/decorator.py
@@ -1,6 +1,57 @@
+from copy import copy
+from enum import Enum
 from typing import no_type_check
 
 
 @no_type_check
 def adt(cls):
+    cls._Key = Enum(
+        '_Key',
+        [k for k in cls.__annotations__.keys() if not k.startswith('__')])
+
+    def _init(self, key, value, orig_init=cls.__init__):
+        self._key = key
+        self._value = value
+        orig_init(self)
+
+    cls.__init__ = _init
+
+    def _repr(self):
+        return f'{type(self)}.{self._key}({self._value})'
+
+    cls.__repr__ = _repr
+
+    def _str(self):
+        return f'<{type(self)}.{self._key}: {self._value}>'
+
+    cls.__str__ = _str
+
+    for caseName, key in cls._Key.__members__.items():
+
+        def constructor(cls, value, _key=key):
+            return cls(key=_key, value=value)
+
+        setattr(cls, caseName, classmethod(constructor))
+
+        def accessor(self, _key=key):
+            if self._key == _key:
+                return self._value
+            else:
+                return None
+
+        setattr(cls, caseName.lower(), accessor)
+
+    def match(self, **kwargs):
+        cases = set(type(self)._Key.__members__.keys())
+        predicates = set(kwargs.keys())
+
+        assert cases == predicates, f'Pattern match on {self} ({predicates}) is over- or under-specified vs. {cases}'
+
+        for key, callback in kwargs:
+            if self._key == type(self)._Key[key.upper()]:
+                return callback(self._value)
+
+        raise ValueError(
+            f'{self} failed pattern match against all of: {predicates}')
+
     return cls

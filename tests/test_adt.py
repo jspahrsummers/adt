fb68092555d8880675ec0a7a55cad3124c0e0bf3
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-06-26 22:35:45 +0100

    Basic EitherADT test

--- tests/test_adt.py
+++ tests/test_adt.py
@@ -23,4 +23,19 @@ class ListADT(Generic[_T]):
 
 
 class TestADT(unittest.TestCase):
-    pass
+    def test_either(self) -> None:
+        e = EitherADT.LEFT(5)
+        self.assertEqual(e, EitherADT.LEFT(5))
+        self.assertNotEqual(e, EitherADT.Right("foobar"))
+        self.assertEqual(e.left, 5)
+        self.assertIsNone(e.right)
+        self.assertEqual(
+            e.match(left=lambda n: n + 1, right=lambda s: s + "z"), 6)
+
+        e = EitherADT.RIGHT("foobar")
+        self.assertNotEqual(e, EitherADT.LEFT(5))
+        self.assertEqual(e, EitherADT.RIGHT("foobar"))
+        self.assertIsNone(e.left)
+        self.assertEqual(e.right, "foobar")
+        self.assertEqual(
+            e.match(left=lambda n: n + 1, right=lambda s: s + "z"), "foobarz")

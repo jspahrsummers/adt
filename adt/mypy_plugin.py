33484243a70f60ea0c20e04b2eb809116c2ca50a
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-06-27 22:24:05 +0100

    WIP plugin

--- adt/mypy_plugin.py
+++ adt/mypy_plugin.py
@@ -1,11 +1,40 @@
 from decimal import Decimal
-from mypy.plugin import Plugin
+from mypy.nodes import AssignmentStmt, ClassDef, NameExpr, Var
+from mypy.plugin import Plugin, ClassDefContext
 
-from typing import Type
+from typing import Callable, Optional, Type
 
 
 class ADTPlugin(Plugin):
-    pass
+    _ADT_DECORATOR = 'adt.decorator.adt'
+
+    def _transform_class(self, context: ClassDefContext) -> None:
+        print(f'_transform_class: {context}')
+
+        cls = context.cls
+        typeInfo = cls.info
+
+        for statement in cls.defs.body:
+            if not isinstance(statement, AssignmentStmt):
+                continue
+
+            for lval in statement.lvalues:
+                if not isinstance(lval, NameExpr):
+                    continue
+
+                node = typeInfo.names[lval.name].node
+                if not isinstance(node, Var):
+                    continue
+
+                print(f'Identified node {node}')
+
+    def get_class_decorator_hook(
+            self,
+            fullname: str) -> Optional[Callable[[ClassDefContext], None]]:
+        if fullname != self._ADT_DECORATOR:
+            return None
+
+        return self._transform_class
 
 
 def plugin(version: str) -> Type[Plugin]:

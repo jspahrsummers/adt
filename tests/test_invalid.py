b98365992604183aff86d71191c8d5e8b508b208
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-06-29 19:25:05 +0100

    Test default value in case
    #4

---
+++ tests/test_invalid.py
@@ -0,0 +1,24 @@
+import unittest
+from typing import Generic, Optional, TypeVar
+
+from adt.decorator import adt
+
+_T = TypeVar('_T')
+
+invalid_defined: bool
+
+try:
+
+    @adt
+    class Invalid(Generic[_T]):
+        CASE: Optional[_T] = None
+
+    invalid_defined = True
+except AttributeError as e:
+    print(f'Expected failure: {e}')
+    invalid_defined = False
+
+
+class TestInvalid(unittest.TestCase):
+    def test_cannotSetDefaultValuesForCases(self) -> None:
+        self.assertFalse(invalid_defined)

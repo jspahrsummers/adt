2dc50870e2213305ddcbac4dc3586a8d38578dd4
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-06-29 10:58:23 +0100

    Define hypothesis profiles to run more examples on CI

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,5 +1,13 @@
-from hypothesis.strategies import integers, one_of, text
+import os
 from typing import Any, NoReturn
+
+import hypothesis
+from hypothesis.strategies import integers, one_of, text
+
+hypothesis.settings.register_profile("ci", max_examples=1000, deadline=100)
+hypothesis.settings.register_profile("dev", max_examples=100, deadline=50)
+hypothesis.settings.load_profile(
+    os.getenv(u'HYPOTHESIS_PROFILE', default='dev'))
 
 any_types = one_of(integers(), text())
 

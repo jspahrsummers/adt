c26e059ef84b9d455b6cbcded0f43f53de9667ca
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-07-03 21:47:27 +0100

    Working case.py

--- adt/case.py
+++ adt/case.py
@@ -4,33 +4,56 @@ _T = TypeVar('_T')
 
 
 class TupleConstructor:
-    def __init__(self, tupleSize: int):
-        self._tupleSize = tupleSize
+    def __init__(self, types: Tuple[Type[Any], ...]):
+        self._types = types
         super().__init__()
 
     def constructCase(self, *args: Any) -> Tuple[Any, ...]:
-        assert len(args) == self._tupleSize
+        assert len(args) == len(self._types)
         return (*args, )
+
+    def __repr__(self) -> str:
+        typeString = ', '.join((str(t) for t in self._types))
+        return f'Case[{typeString}]'
 
 
 class IdentityConstructor:
+    def __init__(self, argType: Type[Any]):
+        self._argType = argType
+        super().__init__()
+
     def constructCase(self, arg: _T) -> _T:
         return arg
 
+    def __repr__(self) -> str:
+        return f'Case[{self._argType}]'
 
-class CaseDefinition:
+
+class CaseConstructor:
     def constructCase(self) -> None:
         return None
 
-    def __getitem__(self, params: Union[Type[Any], Tuple[Type[Any], ...]]
-                    ) -> Union[IdentityConstructor, TupleConstructor]:
-        if isinstance(params, tuple):
-            return TupleConstructor(len(params))
+    def __getitem__(
+            self, params: Union[None, Type[Any], Tuple[Type[Any], ...]]
+    ) -> Union["CaseConstructor", IdentityConstructor, TupleConstructor]:
+        if params is None:
+            return self
+        elif isinstance(params, tuple):
+            return TupleConstructor(params)
         else:
-            return IdentityConstructor()
+            return IdentityConstructor(params)
+
+    def __repr__(self) -> str:
+        return 'Case'
 
 
-Case = CaseDefinition()
-Case
-Case[int]
-Case[int, str]
+# Case = CaseConstructor()
+# Case
+# Case[int]
+# Case[int, str]
+
+# class TestClass:
+#     a: Case
+#     b: Case[None]
+#     c: Case[int]
+#     d: Case[int, str]

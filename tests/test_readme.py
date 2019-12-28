28a640fc7ceb53a5e5b868a8601cbff206a932f0
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-12-28 14:43:07 +0000

    Use fully-qualified import in test_readme

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -4,4 +4,4 @@ import unittest
 class TestMyPyPlugin(unittest.TestCase):
     def test_readmeExamplesAreExecutable(self) -> None:
         """Run a module that uses the examples from the README to test if they execute successfully."""
-        from .source_files import readme_examples
+        from tests.source_files import readme_examples

74c250f824f5897a44d755eabe12e141483f29ba
wchresta <34962284+wchresta@users.noreply.github.com>
2019-12-28 12:47:19 -0500

    Activate issue unit-tests as expected failures

--- tests/test_mypy_plugin.py
+++ tests/test_mypy_plugin.py
@@ -3,28 +3,26 @@ import mypy.main
 import mypy.version
 import os
 import sys
-import io
-from typing import List, Optional
 
 
 class TestMyPyPlugin(unittest.TestCase):
     def test_issue21(self) -> None:
         self._call_mypy_on_source_file("issue21.py")
 
-    def _test_issue25(self) -> None:
-        # TODO: This is a deactivated test for issue #25.
+    @unittest.expectedFailure  # Issue #26 is still unfixed
+    def test_issue25(self) -> None:
         # Activate it when working on it.
         # cf. https://github.com/jspahrsummers/adt/issues/25
         self._call_mypy_on_source_file("issue25.py")
 
-    def _test_issue26(self) -> None:
-        # TODO: This is a deactivated test for issue #26.
+    @unittest.expectedFailure  # Issue #26 is still unfixed
+    def test_issue26(self) -> None:
         # Activate it when working on it.
         # cf. https://github.com/jspahrsummers/adt/issues/26
         self._call_mypy_on_source_file("issue26.py")
 
-    def _test_readme_examples(self) -> None:
-        # TODO: This fails because of issue #26. Deactivated this test.
+    @unittest.expectedFailure  # Fails because issue #26 is still unfixed
+    def test_readme_examples(self) -> None:
         self._call_mypy_on_source_file("readme_examples.py")
 
     def _call_mypy_on_source_file(self, source_file_name: str) -> None:

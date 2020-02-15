a0a9315255aef5f387e816093061ed58d0d2f6c7
slavoie <60849265+Sebastien-Lavoie@users.noreply.github.com>
2020-02-15 10:13:20 -0500

    Add private field to access declared types

--- adt/decorator.py
+++ adt/decorator.py
@@ -27,6 +27,18 @@ def adt(cls):
     cls._Key = Enum(  # type: ignore
         '_Key', list(caseConstructors.keys()))
 
+    values = list(caseConstructors.values())
+    types = []
+    for i in range(len(caseConstructors.values())):
+        try:
+            types.append(values[i]._argType)
+        except AttributeError:
+            try:
+                types.append(values[i]._types)
+            except AttributeError:
+                types.append(None)
+    cls._types = types
+
     _installInit(cls)
     _installRepr(cls)
     _installStr(cls)

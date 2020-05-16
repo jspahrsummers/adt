6be74e66f1d04e8956457b43cd3a97f3a82f621f
adewindt <aaron.dewindt@gmail.com>
2020-05-16 06:53:42 +0200

    Probably fixed `Item "None" of "Optional[FrameType]" has no attribute "f_back"` error and missing return statement.

--- adt/decorator.py
+++ adt/decorator.py
@@ -126,14 +126,17 @@ class Accessor:
     def __enter__(self) -> Any:
         if self.adt._key != self.case:
             sys.settrace(lambda *args, **keys: None)
-            frame = inspect.currentframe().f_back
-            setattr(frame, "f_trace", self.trace)
+            frame = inspect.currentframe()
+            if frame:
+                setattr(frame.f_back, "f_trace", self.trace)
         else:
             return self.adt._value
 
     def __exit__(self, exc_type: type, exc_val: Exception, exc_tb: Any) -> Optional[bool]:
         if self.SkipCase is exc_type:
             return True
+        else:
+            return
 
 
 def _installOneAccessor(instance: Any, case: Enum) -> None:
